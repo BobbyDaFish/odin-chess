db961811c85856dc0406076699b05d0a1491951b
BobbyDaFish <130507595+BobbyDaFish@users.noreply.github.com>
2024-07-09 15:02:34 -0600

    Add method for calling possible moves
    Method calls the correct method for specific piece types based on their
    icon. Created method to generate possible king moves. Need to create
    method to check for, and remove, invalid moves then return that array

--- lib/chess.rb
+++ lib/chess.rb
@@ -26,12 +26,43 @@ class Chess
     valid_piece = false
     piece = []
     while valid_piece == false
-      puts 'Select your piece!\nPick a row from A to Z'
+      puts "Select your piece!\nPick a row from A to Z"
       piece[0] = gets.chomp
       puts 'Pick a column from 1 to 8'
       piece[1] = gets.chomp.to_i
       valid_piece = true if player.value?(piece)
     end
     piece
+  end
+
+  def choose_move(piece_coords)
+    move = []
+    puts "Where are you moving?\nPick a row from A to Z"
+    move[0] = gets.chomp
+    puts 'Pick a column from 1 to 8'
+    move[1] = gets.chomp.to_i
+    return move if possible_moves(@current_turn, piece_coords).any?(move)
+
+    puts 'Invalid move, try again.'
+    false
+  end
+
+  def possible_moves(player, piece_coords) # rubocop:disable Metrics/AbcSize,Metrics/CyclomaticComplexity
+    row_to_num = { 'a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5, 'f' => 6, 'g' => 7, 'h' => 8 }
+    true_pos = [row_to_num[piece_coords[0]], piece_coords[1]]
+    piece_type = (player.pieces.pieces.select { |_k, h| h[:position] == piece_coords }).flatten[1][:icon]
+
+    king_moves(true_pos) if ["\u2654 ", "\u265a "].include?(piece_type)
+    queen_moves(true_pos) if ["\u2655 ", "\u265b "].include?(piece_type)
+    knight_moves(true_pos) if ["\u2658 ", "\u265e "].include?(piece_type)
+    bishop_moves(true_pos) if ["\u2657 ", "\u265d "].include?(piece_type)
+    rook_moves(true_pos) if ["\u2656 ", "\u265c "].include?(piece_type)
+    pawn_moves(true_pos) if ["\u2659 ", "\u265f "].include?(piece_type)
+  end
+
+  def king_move(piece_coords)
+    all_moves = [[-1, 0], [-1, 1], [0, 1], [1, 1], [1, 0], [1, -1], [0, -1], [-1, -1]]
+    possible_moves = []
+    possible_moves << all_moves.each { |arr| [arr[0] + piece_coords[0], arr[1] + piece_coords[1]] }
   end
 end
--- spec/chess_spec.rb
+++ spec/chess_spec.rb
@@ -15,4 +15,37 @@ describe Chess do
       expect(array).to eql(['a', 1])
     end
   end
+
+  describe '#choose_move' do
+    describe 'user inputs a valid move selection' do
+      before do
+        allow(game).to receive(:possible_moves).and_return([['c', 1], ['d', 1]])
+        allow(game).to receive(:gets).and_return('d', '1')
+      end
+
+      it 'returns the selection of a valid move' do
+        move = game.choose_move(['b', 1])
+        expect(move).to eql(['d', 1])
+      end
+    end
+
+    describe 'user inputs invalid selection' do
+      before do
+        allow(game).to receive(:possible_moves).and_return([['c', 1], ['d', 1]])
+        allow(game).to receive(:gets).and_return('e', '1')
+      end
+
+      it 'returns false' do
+        move = game.choose_move(['b', 1])
+        expect(move).to be false
+      end
+    end
+  end
+
+  describe '#possible_moves' do
+    it 'returns array of possible moves as sub arrays' do
+      moves = game.possible_moves('white', ['b', 1])
+      expect(moves).to eql([['c', 1], ['d', 1]])
+    end
+  end
 end
